d3be1559d442aeb4dc9464fb62bb54c6587a00a1
Sandip Sadhukhan <sandippakhanna@gmail.com>
2021-12-31 14:43:27 +0530

    update

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,8 @@
-document.getElementById("downloadBtn").addEventListener("click", () => {
+document.getElementById("downloadBtn").addEventListener("click", (e) => {
+  // disabled button
+  document.getElementById("downloadBtn").disabled = true;
   document.getElementById("my_iframe").src = "./static/files/steganography.exe";
+  document.getElementById("downloadBtn").innerHTML = `
+  Download Starting <i class="bi bi-cloud-download-fill ms-2"></i>
+  `;
 });
